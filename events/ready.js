464d20abaacbcbfead87d60d242099315b42239a
EgemenSSS <93443004+EgemenSSS@users.noreply.github.com>
2022-08-29 11:42:50 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (client) => {//youtube/NoblesYT
-  console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Youtube/NoblesYT", { //Oynuyor Kısmı
-    type: "WATCHING",//LISTENING, WATCHING, PLAYING, STREAMING
-  });
-};
